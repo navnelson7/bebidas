d7e065038c80e305dc3410061a4ba1cbbcae526b
Juan Navarro <navnelson7@gmail.com>
2020-07-07 22:49:44 -0600

    Agregando RecetasContext al proyecto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,24 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import {CategoriasContext} from '../context/CategoriasContext';
+import {RecetasContext} from '../context/RecetasContext';
 const Formulario = () => {
-     const {categorias} = useContext(CategoriasContext);
+    const [busqueda, guardarBusqueda] = useState({
+        nombre: "",
+        categoria: ''
+    })
+     const {categorias} = useContext(CategoriasContext);     
+     const {buscarRecetas} = useContext({RecetasContext});
+     //funcion para leeer los contenidos 
+     const obtenerDatosReceta = e => {
+         guardarBusqueda({
+             ...busqueda,
+             [e.target.name] : e.target.value
+         })
+     }
     return (
         <form
             className="col-12"
+            onSubmit={buscarRecetas(busqueda)}
         >
             <fieldset className="text-center">
                 <legend>Busca bebidas por caegorias</legend>
@@ -16,12 +30,14 @@ const Formulario = () => {
                         className="form-control"
                         type="text"
                         placeholder="Buscar por ingrediente"
+                        onChange={obtenerDatosReceta}
                     />
                 </div>
                 <div className="col-md-4">
                     <select
                         className="form-control"
                         name="categoria"
+                        onChange={obtenerDatosReceta}
                     >
                         <option>-- Selecciona Categoria</option>
                         {categorias.map(categoria =>(
